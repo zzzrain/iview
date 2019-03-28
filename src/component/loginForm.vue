137353a26a04f07291c3b8510438b5d81bae2246
huzirong <1594087542@qq.com>
2019-03-28 18:13:47 +0800

    --'init'

---
+++ src/component/loginForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <Form ref="loginForm" >
+        <FormItem prop="userName">
+            <Input placeholder="请输入用户名">
+        <span slot="prepend">
+          <Icon :size="16" type="ios-person"></Icon>
+        </span>
+            </Input>
+        </FormItem>
+        <FormItem prop="password">
+            <Input type="password" placeholder="请输入密码">
+        <span slot="prepend">
+          <Icon :size="14" type="md-lock"></Icon>
+        </span>
+            </Input>
+        </FormItem>
+        <FormItem>
+            <Button type="primary" long>登录</Button>
+        </FormItem>
+    </Form>
+</template>
+
+<script>
+    export default {
+        name: "loginForm"
+    }
+</script>
+
+<style scoped>
+
+</style>
